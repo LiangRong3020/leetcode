dcc79aba810c84fcf518cf2d639b48500619303a
張良榮 <zhangliangrong@237-87-116-150-static.chief.net.tw>
2021-07-22 12:35:52 +0800

    optimize 838

--- python/838_Push_Dominoes.py
+++ python/838_Push_Dominoes.py
@@ -1,66 +1,88 @@
 class Solution(object):
+
+    def cmp(self, a, b):
+        return (a > b) - (a < b)
+
     def pushDominoes(self, dominoes):
-        """
-        :type dominoes: str
-        :rtype: str
-        """
-        dominoes_count = len(dominoes)
+        symbols = [(i, x) for i, x in enumerate(dominoes) if x != '.']
+        symbols = [(-1, 'L')] + symbols + [(len(dominoes), 'R')]
 
-        last_push = -1
-        push_label = 'None'
+        ans = list(dominoes)
+        for (i, x), (j, y) in zip(symbols, symbols[1:]):
+            if x == y:
+                for k in range(i+1, j):
+                    ans[k] = x
+            elif x > y: #RL
+                for k in range(i+1, j):
+                    ans[k] = '.LR'[self.cmp(k-i, j-k)]
 
-        finnal_state = ''
-        for i in range(dominoes_count):
-            now_domino = dominoes[i]
-            if now_domino == '.' and push_label in ('L', 'None'):
-                j = i + 1
-                not_Decide = True
-                while j < dominoes_count:
-                    if dominoes[j] == 'R':
-                        break
-                    elif dominoes[j] == 'L':
-                        finnal_state += 'L'
-                        not_Decide = False
-                        break
-
-                    j += 1
-                if not_Decide:
-                    finnal_state += now_domino
+        return "".join(ans)
 
 
-            elif now_domino == '.' and push_label == 'R':
-                j = i + 1
-                not_Decide = True
-                while j < dominoes_count:
-                    if dominoes[j] == 'R':
-                        finnal_state += 'R'
-                        not_Decide = False
-                        break
-                    elif dominoes[j] == 'L':
-                        right_distinct = i - last_push
-                        left_distinct = j - i
 
-                        if right_distinct > left_distinct:
-                            finnal_state += 'L'
-                            not_Decide = False
-                            break
-                        elif left_distinct > right_distinct:
-                            finnal_state += 'R'
-                            not_Decide = False
-                            break
-                        else:
-                            finnal_state += '.'
-                            not_Decide = False
-                            break
-                    j += 1
-
-                if not_Decide:
-                    finnal_state += 'R'
-
-
-            else:
-                finnal_state += now_domino
-                last_push = i
-                push_label = now_domino
-
-        return finnal_state
+# class Solution(object):
+#     def pushDominoes(self, dominoes):
+#         """
+#         :type dominoes: str
+#         :rtype: str
+#         """
+#         dominoes_count = len(dominoes)
+#
+#         last_push = -1
+#         push_label = 'None'
+#
+#         finnal_state = ''
+#         for i in range(dominoes_count):
+#             now_domino = dominoes[i]
+#             if now_domino == '.' and push_label in ('L', 'None'):
+#                 j = i + 1
+#                 not_Decide = True
+#                 while j < dominoes_count:
+#                     if dominoes[j] == 'R':
+#                         break
+#                     elif dominoes[j] == 'L':
+#                         finnal_state += 'L'
+#                         not_Decide = False
+#                         break
+#
+#                     j += 1
+#                 if not_Decide:
+#                     finnal_state += now_domino
+#
+#
+#             elif now_domino == '.' and push_label == 'R':
+#                 j = i + 1
+#                 not_Decide = True
+#                 while j < dominoes_count:
+#                     if dominoes[j] == 'R':
+#                         finnal_state += 'R'
+#                         not_Decide = False
+#                         break
+#                     elif dominoes[j] == 'L':
+#                         right_distinct = i - last_push
+#                         left_distinct = j - i
+#
+#                         if right_distinct > left_distinct:
+#                             finnal_state += 'L'
+#                             not_Decide = False
+#                             break
+#                         elif left_distinct > right_distinct:
+#                             finnal_state += 'R'
+#                             not_Decide = False
+#                             break
+#                         else:
+#                             finnal_state += '.'
+#                             not_Decide = False
+#                             break
+#                     j += 1
+#
+#                 if not_Decide:
+#                     finnal_state += 'R'
+#
+#
+#             else:
+#                 finnal_state += now_domino
+#                 last_push = i
+#                 push_label = now_domino
+#
+#         return finnal_state
