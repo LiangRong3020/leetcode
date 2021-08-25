2c27d2989d79ac9a41210b6706afbffd410d25fb
張良榮 <juce810282@gmail.com>
2021-08-25 21:02:45 +0800

    add 633

---
+++ python/633_Sum_of_Square_Numbers.py
@@ -0,0 +1,22 @@
+import math
+
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+
+        helf_number= c // 2
+
+        s= 0
+
+        while s ** 2 <= helf_number:
+            if math.sqrt(c- s ** 2) % 1 == 0:
+                return True
+
+            s += 1
+
+        return False
+    
+
